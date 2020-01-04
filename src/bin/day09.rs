71979c4d738051eaa49183b4bfe3de8a19a2712f
Brandon Mittman <BrandonMittman@gmail.com>
2020-01-03 23:20:23 -0700

    Day 9 part 1

---
+++ src/bin/day09.rs
@@ -0,0 +1,28 @@
+use advent_of_code_2019::*;
+use std::sync::mpsc::channel;
+
+fn main() {
+    let input: Vec<isize> = get_input()
+        .trim()
+        .split(',')
+        .map(|i| i.parse::<isize>().unwrap())
+        .collect();
+
+    println!("Test 1: {:?}", run_test1(&input.clone()));
+    //println!("Test 2: {:?}", run_test2(&input.clone()));
+}
+
+fn run_test1(input: &Vec<isize>) -> Vec<isize> {
+    let mut prog = input.clone();
+    let (tx, rx) = channel();
+    tx.send(1).unwrap();
+    run_tape(&mut prog, rx, tx)
+}
+
+fn run_test2(input: &Vec<isize>) -> Vec<isize> {
+    let mut prog = input.clone();
+    let (tx, rx) = channel();
+    tx.send(5).unwrap();
+    run_tape(&mut prog, rx, tx)
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Read;
 use std::sync::mpsc::{Receiver, Sender};
@@ -10,46 +11,70 @@ pub fn get_input() -> String {
     buf
 }
 
+/// Run the given tape through our Intcode computer. The input is a list of integers. The list of
+/// integers contains opcodes which determine what to do with the following data.
+///
+/// The opcode format is 'ABCDE', where:
+/// DE - two-digit opcode,
+/// C - mode of 1st parameter
+/// B - mode of 2nd parameter
+/// C - mode of 3rd parameter
+///
+/// Current supported opcodes (and number of parameters) are:
+/// 1 (3)- Add
+/// 2 (3)- Subtract
+/// 3 (1)- Use input
+/// 4 (1)- Produce output
+/// 5 (2)- Jump if true
+/// 6 (2)- Jump if false
+/// 7 (3)- Less than
+/// 8 (3)- Equals
+/// 99 (0) - Halt
+///
+/// The two parameter modes are position mode (0) and immediate mode (1). Position mode indicates
+/// that the parameter is a location to store/retreive the data from. Immediate mode indicates that
+/// the paramater is the value to be used.
 pub fn run_tape(tape: &mut [isize], input: Receiver<isize>, output: Sender<isize>) -> Vec<isize> {
+    let mut tape: HashMap<usize, isize> = tape.iter().enumerate().map(|(i, &v)| (i, v)).collect();
     let mut ip = 0;
+    let mut relative_base = 0;
     let mut output_vec = Vec::new();
     loop {
-        let (op_code, first_mode, second_mode, _third_mode) = get_op_code_and_param_mode(tape[ip]);
+        let (op_code, first_mode, second_mode, third_mode) =
+            get_op_code_and_param_mode(*tape.entry(ip).or_insert(0));
         match op_code {
-            99 => break,
+            99 => {
+                break;
+            }
             1 => {
                 // Add
-                let op1 = get_val_with_mode(tape, ip + 1, first_mode);
-                let op2 = get_val_with_mode(tape, ip + 2, second_mode);
+                // Add first two parameters and store in third
+                let op1 = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
+                let op2 = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode);
 
-                let out = tape[ip + 3] as usize;
-
-                tape[out] = op1 + op2;
+                insert_val_with_mode(&mut tape, relative_base, ip + 3, third_mode, op1 + op2);
                 ip += 4;
             }
             2 => {
                 // Subtract
-                let op1 = get_val_with_mode(tape, ip + 1, first_mode);
-                let op2 = get_val_with_mode(tape, ip + 2, second_mode);
+                // Subtract first two parameters and store in third
+                let op1 = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
+                let op2 = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode);
 
-                let out = tape[ip + 3] as usize;
-
-                tape[out] = op1 * op2;
+                insert_val_with_mode(&mut tape, relative_base, ip + 3, third_mode, op1 * op2);
                 ip += 4;
             }
             3 => {
                 // Input
+                // Take value and store in the given parameter
                 let val = input.recv().unwrap();
-                tape[tape[ip + 1] as usize] = val;
+                insert_val_with_mode(&mut tape, relative_base, ip + 1, first_mode, val);
                 ip += 2;
             }
             4 => {
                 // Output
-                let print = if first_mode == 0 {
-                    tape[tape[ip + 1] as usize]
-                } else {
-                    tape[ip + 1]
-                };
+                // Output the value in the given parameter
+                let print = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
 
                 // Don't care about this error since we'll still push to the vec
                 // and return that
@@ -59,37 +84,60 @@ pub fn run_tape(tape: &mut [isize], input: Receiver<isize>, output: Sender<isize
             }
             5 => {
                 // jump-if-true
-                if get_val_with_mode(tape, ip + 1, first_mode) != 0 {
-                    ip = get_val_with_mode(tape, ip + 2, second_mode) as usize;
+                // If first parameter is non-zero, set instruction pointer to the second parameter
+                if get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode) != 0 {
+                    ip = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode) as usize;
                 } else {
                     ip += 3;
                 }
             }
             6 => {
                 // jump-if-false
-                if get_val_with_mode(tape, ip + 1, first_mode) == 0 {
-                    ip = get_val_with_mode(tape, ip + 2, second_mode) as usize;
+                // If first parameter is zero, set instruction pointer to the second parameter
+                if get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode) == 0 {
+                    ip = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode) as usize;
                 } else {
                     ip += 3;
                 }
             }
             7 => {
                 // less than
-                let op1 = get_val_with_mode(tape, ip + 1, first_mode);
-                let op2 = get_val_with_mode(tape, ip + 2, second_mode);
+                // If first parameter is less than the second, store a 1 in the third parameter,
+                // otherwise 0
+                let op1 = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
+                let op2 = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode);
 
-                let out = tape[ip + 3] as usize;
-                tape[out] = if op1 < op2 { 1 } else { 0 };
+                insert_val_with_mode(
+                    &mut tape,
+                    relative_base,
+                    ip + 3,
+                    third_mode,
+                    if op1 < op2 { 1 } else { 0 },
+                );
                 ip += 4;
             }
             8 => {
                 // equals
-                let op1 = get_val_with_mode(tape, ip + 1, first_mode);
-                let op2 = get_val_with_mode(tape, ip + 2, second_mode);
+                // If first parameter is equal to the second, store a 1 in the third parameter,
+                // otherwise 0
+                let op1 = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
+                let op2 = get_val_with_mode(&mut tape, relative_base, ip + 2, second_mode);
 
-                let out = tape[ip + 3] as usize;
-                tape[out] = if op1 == op2 { 1 } else { 0 };
+                insert_val_with_mode(
+                    &mut tape,
+                    relative_base,
+                    ip + 3,
+                    third_mode,
+                    if op1 == op2 { 1 } else { 0 },
+                );
                 ip += 4;
+            }
+            9 => {
+                // relative base offset
+                // Adjust the relative base by the value of the parameter
+                let op1 = get_val_with_mode(&mut tape, relative_base, ip + 1, first_mode);
+                relative_base += op1;
+                ip += 2;
             }
             _ => panic!("Invalid OpCode value: {}!", op_code),
         }
@@ -98,14 +146,61 @@ pub fn run_tape(tape: &mut [isize], input: Receiver<isize>, output: Sender<isize
     output_vec
 }
 
-pub fn get_val_with_mode(tape: &[isize], pos: usize, mode: usize) -> isize {
-    if mode == 0 {
-        tape[tape[pos] as usize]
-    } else {
-        tape[pos]
+/// Use the mode to determine which value of the tape to use.
+/// Supported modes:
+/// 0 -> Position - The parameter is given as a position in the tape.
+/// 1 -> Immediate - The parameter is given
+/// 2 -> Relative - The parameter is relative to the relative base
+pub fn get_val_with_mode(
+    tape: &mut HashMap<usize, isize>,
+    rb: isize,
+    pos: usize,
+    mode: usize,
+) -> isize {
+    match mode {
+        0 => {
+            let key = *tape.entry(pos).or_insert(0) as usize;
+            *tape.entry(key).or_insert(0)
+        }
+        1 => *tape.entry(pos).or_insert(0),
+        2 => {
+            let key = *tape.entry(pos).or_insert(0);
+            *tape.entry((key + rb) as usize).or_insert(0)
+        }
+        _ => panic!("Unsupported mode!"),
     }
 }
 
+/// Use the mode to determine which value of the tape to use.
+/// Supported modes:
+/// 0 -> Position - The parameter is given as a position in the tape.
+/// 1 -> Immediate - The parameter is given
+/// 2 -> Relative - The parameter is relative to the relative base
+pub fn insert_val_with_mode(
+    tape: &mut HashMap<usize, isize>,
+    rb: isize,
+    pos: usize,
+    mode: usize,
+    value: isize,
+) {
+    match mode {
+        0 => {
+            let key = *tape.entry(pos).or_insert(0) as usize;
+            tape.insert(key, value);
+        }
+        2 => {
+            let key = *tape.entry(pos).or_insert(0);
+            tape.insert((key + rb) as usize, value);
+        }
+        _ => panic!("Unsupported mode!"),
+    }
+}
+
+/// The opcode format is 'ABCDE', where:
+/// DE - two-digit opcode,
+/// C - mode of 1st parameter
+/// B - mode of 2nd parameter
+/// C - mode of 3rd parameter
 fn get_op_code_and_param_mode(val: isize) -> (usize, usize, usize, usize) {
     let op_code = (val % 100) as usize;
     let first_mode = ((val / 100) % 10) as usize;
